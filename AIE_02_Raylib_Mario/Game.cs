7393085a4a34aa8bc20c6aa6faaf52afd13f8b8c
Aaron Cox <aaron.cox@aie.edu.au>
2021-02-01 15:10:55 +1100

    initial commit

---
+++ AIE_02_Raylib_Mario/Game.cs
@@ -0,0 +1,48 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIE_02_Raylib_Mario
+{
+    class Game
+    {
+        public int windowWidth = 800;
+        public int windowHeight = 450;
+        public string windowTitle = "Mario Walkthrough";
+
+        Texture2D marioTexture;
+        float marioXPos = 400;
+        float marioYPos = 200;
+        float marioWidth = 32;
+        float marioHeight = 32;
+
+        public void LoadGame()
+        {
+            // TODO: Load game assets here
+            marioTexture = Raylib.LoadTexture("./assets/mario_1.png");
+        }
+
+        public void Update(float deltaTime)
+        {
+            // TODO: Update related logic here
+        }
+
+        public void Draw()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.RAYWHITE);
+
+            // TODO: Drawing related logic here
+
+            // draws some text
+            Raylib.DrawText("Mario", 10, 10, 32, Color.DARKGRAY);
+
+            // draws a rotating texture in center of screen
+            RayLibExt.DrawTexture(marioTexture, marioXPos, marioYPos, marioWidth, marioHeight,
+                Color.WHITE, 0, 0.5f, 1.0f);
+
+            Raylib.EndDrawing();
+        }
+    }
+}
